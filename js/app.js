dcd6956e679dded03c9c20b36c8bac93a59e5311
Edinjson <figueroaedinson@gmail.com>
2020-05-20 22:25:42 -0500

    agregar, eliminar y mostrar de localStorage

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 
 //variables
 const cursos = document.querySelector('#lista-cursos');
-//const carrito;
+const carrito = document.querySelector("#carrito");
 const listCarrito = document.querySelector("#lista-carrito tbody");
+const vaciarCarrito = document.querySelector("#vaciar-carrito");
 
 
 
@@ -10,7 +11,14 @@ const listCarrito = document.querySelector("#lista-carrito tbody");
 cargarEventListeners();
 
 function cargarEventListeners(){
+    //clic en agregar curso
     cursos.addEventListener('click', addCurso);
+    //clic en eliminar cuerso
+    carrito.addEventListener('click', eliminarCurso);
+    //vaciar carrito
+    vaciarCarrito.addEventListener('click', vaciarLista);
+
+    mostrarLocalStorage();
     
 
 }
@@ -51,7 +59,7 @@ function leerDatosSeleccion(curso){
 function addCarrito(infoCurso){
 
     const row = document.createElement('tr');
-    console.log(row);
+    
 
     row.innerHTML = `<td>
                     <img src="${infoCurso.imagen}" width="100">
@@ -70,4 +78,100 @@ function addCarrito(infoCurso){
 
     listCarrito.appendChild(row);
 
+    addLocalStorage(infoCurso);
+
+}
+
+//mostrar carritode localStorage
+function mostrarLocalStorage(){
+
+        let cursoArray;
+        cursoArray = buscarLocalStorage();
+        
+        cursoArray.forEach(function(curso){
+            
+            const row = document.createElement('tr');  
+
+            row.innerHTML = `<td>
+                            <img src="${curso.imagen}" width="100">
+                            </td>
+                            <td>
+                            ${curso.titulo}
+                            </td>
+                            <td>
+                            ${curso.precio}
+                            </td>
+                            <td>
+                            <a src"#" class="borrar-curso" data-id="${curso.id}">
+                            X</a>
+                            </td>`
+
+
+            listCarrito.appendChild(row);
+
+        });
+}
+
+// agregar a local storage
+function addLocalStorage(infoCurso){
+    let cursoArray;
+    console.log(infoCurso);
+    cursoArray = buscarLocalStorage();
+
+    cursoArray.push(infoCurso);
+    localStorage.setItem('curso', JSON.stringify(cursoArray));
+
+}
+
+//cosulytar de localStorage
+function buscarLocalStorage(){
+    let cursoArray;
+    if(localStorage.getItem('curso') === null){
+        cursoArray = [];
+    }else{
+        cursoArray = JSON.parse(localStorage.getItem('curso'));
+    }
+    return cursoArray;
+
+}
+
+//eliminar curso
+function eliminarCurso(e){
+    e.preventDefault();
+    if(e.target.classList.contains('borrar-curso')){
+        e.target.parentElement.parentElement.remove();
+
+        
+        //eliminar de localStorage
+        eliminarLocalStorage(e.target.getAttribute('data-id'));
+    }
+}
+
+//eliminar todo
+function vaciarLista(e){
+    e.preventDefault();
+    //mientras siga habiendo elementos -> eliminar
+    while(listCarrito.firstChild){
+        listCarrito.removeChild(listCarrito.firstChild);
+    }
+    localStorage.clear();
+}
+
+
+//eliminar de localStorahe
+function eliminarLocalStorage(id){
+    let listCurso;
+
+    listCurso = buscarLocalStorage();
+
+    listCurso.forEach(function(curso, index){
+        if(curso.id === id){
+            console.log("igua a "+id);
+            listCurso.splice(index,1);
+        
+        }
+    });
+    localStorage.setItem('curso',JSON.stringify(listCurso));
+    console.log(listCurso);
+    
 }
